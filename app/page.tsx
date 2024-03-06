7b05e4ed97f1e8e16b328353398d17fa0f04071a
Mostafa Eissa <moeissa410@gmail.com>
2024-03-06 21:20:13 +0200

    3

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,11 @@
 "use client";
-// import { getServerSession } from "next-auth";
-// import { options } from "./api/auth/[...nextauth]/options";
 
 import React, { useEffect } from "react";
-import { redirect, useRouter } from "next/navigation";
-import { useSession } from "next-auth/react";
 
 const Home = () => {
-  const router = useRouter();
-
-  const { data: session, status: sessionStatus } = useSession();
-  useEffect(() => {
-    if (sessionStatus === "authenticated") {
-      router.replace("/");
-    }
-  }, [sessionStatus, router]);
   return (
     <div>
       <h1>Member Server Session</h1>
-      <p>{session?.user?.email}</p>
     </div>
   );
 };
