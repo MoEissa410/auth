987cbcc708770eb48c3c643adb512e969f545c2b
Mostafa Eissa <moeissa410@gmail.com>
2024-03-07 18:00:51 +0200

    dd

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "./(components)/Nav";
-import AuthProvider from "./(components)/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,12 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <AuthProvider>
-        <body className={`${inter.className} overflow-hidden`}>
-          <Nav />
-          {children}
-        </body>
-      </AuthProvider>
+      <body className={`${inter.className} overflow-hidden`}>
+        <Nav />
+        {children}
+      </body>
     </html>
   );
 }
